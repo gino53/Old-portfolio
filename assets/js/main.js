7b16dac621736ad2b1be8d5124b3f593167447f0
gino53 <castagnagino5@gmail.com>
2022-11-16 02:33:41 +0100

    cursor, menu, vanilla, profil

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,9 @@
+const cursor = document.querySelector('.cursor');
+
+document.addEventListener('mousemove', e => {
+    cursor.setAttribute('style', 'top:'+(e.pageY - 20)+"px; left:"+(e.pageX - 20)+"px;")
+})
+
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 const sections = document.querySelectorAll('section[id]')
     
@@ -18,3 +24,16 @@ const scrollActive = () =>{
 	})
 }
 window.addEventListener('scroll', scrollActive)
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    reset: true,
+    distance: '60px',
+    duration: 2500,
+    delay: 400
+})
+
+sr.reveal(`.home, .skills, .projets, .contact`, { delay: 600, origin: 'top' });
+sr.reveal(`.profil-picture`, { delay: 1000, origin: 'bottom' });
+sr.reveal(`.block-1-img, .block-2-img, .block-3-img, .block-4-img`, { delay: 700, origin: 'right' });
+sr.reveal(`.home-text-1, .home-text-2, .home-text-3, .home-text-4`, { delay: 700, origin: 'left', interval: 200 });
