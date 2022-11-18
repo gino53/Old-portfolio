aff5b1ae2bea7d3fe003f16bd72cac8c803cc5a7
gino53 <castagnagino5@gmail.com>
2022-11-18 01:15:28 +0100

    skills

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,39 +1,42 @@
 const cursor = document.querySelector('.cursor');
 
 document.addEventListener('mousemove', e => {
-    cursor.setAttribute('style', 'top:'+(e.pageY - 20)+"px; left:"+(e.pageX - 20)+"px;")
+	cursor.setAttribute('style', 'top:' + (e.pageY - 15) + "px; left:" + (e.pageX - 15) + "px;")
 })
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 const sections = document.querySelectorAll('section[id]')
-    
-const scrollActive = () =>{
-  	const scrollY = window.pageYOffset
 
-	sections.forEach(current =>{
+const scrollActive = () => {
+	const scrollY = window.pageYOffset;
+
+	sections.forEach(current => {
 		const sectionHeight = current.offsetHeight,
-			  sectionTop = current.offsetTop - 58,
-			  sectionId = current.getAttribute('id'),
-			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+			sectionTop = current.offsetTop - 58,
+			sectionId = current.getAttribute('id'),
+			sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
 
-		if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+		if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
 			sectionsClass.classList.add('active-link')
-		}else{
+		} else {
 			sectionsClass.classList.remove('active-link')
-		}                                                    
+		}
 	})
 }
 window.addEventListener('scroll', scrollActive)
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
 const sr = ScrollReveal({
-    reset: true,
-    distance: '60px',
-    duration: 2500,
-    delay: 400
+	reset: true,
+	distance: '60px',
+	duration: 2500,
+	delay: 400
 })
 
 sr.reveal(`.home, .skills, .projets, .contact`, { delay: 600, origin: 'top' });
 sr.reveal(`.profil-picture`, { delay: 1000, origin: 'bottom' });
 sr.reveal(`.block-1-img, .block-2-img, .block-3-img, .block-4-img`, { delay: 700, origin: 'right' });
 sr.reveal(`.home-text-1, .home-text-2, .home-text-3, .home-text-4`, { delay: 700, origin: 'left', interval: 200 });
+sr.reveal(`.progress`, { delay: 700, origin: 'left', duration: 4000, distance: '140px' });
+sr.reveal(`.left`, { delay: 700, origin: 'left', interval: 200 });
+sr.reveal(`.right`, { delay: 700, origin: 'right', interval: 200 });
